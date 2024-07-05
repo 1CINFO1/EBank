9509355ee9bd667eb07326ab2f5b16195c1a759b
saif jouini <saifeddine.jouini@vista.com>
2024-07-05 16:19:43 +0100

    user role is now seted up, along with project arch add it the login Service

--- src/main/java/com/ebank/application/models/CharityCampaignModel.java
+++ src/main/java/com/ebank/application/models/CharityCampaignModel.java
@@ -2,8 +2,9 @@ package com.ebank.application.models;
 
 import java.time.LocalDate;
 
-public class CharityCampaignModel extends User{
+public class CharityCampaignModel extends User {
     private String CompagnieDeDon_Patente;
+    private final String role = "CHARITY";
 
     public CharityCampaignModel(String name, String email, LocalDate dob, int acc_num, double balance, String password, String compagnieDeDon_Patente) {
         super(name, email, dob, acc_num, balance, password);
@@ -11,19 +12,23 @@ public class CharityCampaignModel extends User{
     }
 
     public CharityCampaignModel() {
+        super();
     }
 
     public CharityCampaignModel(String name, String userEmail, LocalDate dob, int accNum, double balance, String compagnieDeDon_Patente) {
-        super(name,userEmail,dob,accNum,balance);
+        super(name, userEmail, dob, accNum, balance);
         CompagnieDeDon_Patente = compagnieDeDon_Patente;
     }
 
     public String getCompagnieDeDon_Patente() {
-
         return CompagnieDeDon_Patente;
     }
 
     public void setCompagnieDeDon_Patente(String compagnieDeDon_Patente) {
         CompagnieDeDon_Patente = compagnieDeDon_Patente;
+    }
+
+    public String getRole() {
+        return role;
     }
 }
